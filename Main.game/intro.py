2834ebfc8d218ecd429648a72778dd6648fcb8e6
onnik <onni.kivinen@gmail.com>
2024-02-26 20:19:01 +0200

    Refactor intro and airplane_model_choice functions

--- Main.game/intro.py
+++ Main.game/intro.py
@@ -1,32 +1,46 @@
+#-------------------------------------------------------------------------------
+# ONNIN SPAGETTI KOODIA
+
 def intro(): # Intro funktio TestiTestiTesti
     print('''
- \ \      / /__| | ___ ___  _ __ ___   ___| |
-  \ \ /\ / / _ \ |/ __/ _ \| '_ ` _ \ / _ \ |
-   \ V  V /  __/ | (_| (_) | | | | | |  __/_|
-    \_/\_/ \___|_|\___\___/|_| |_| |_|\___(_)''')
+          
+        \ \      / /__| | ___ ___  _ __ ___   ___| |
+         \ \ /\ / / _ \ |/ __/ _ \| '_ ` _ \ / _ \ |
+          \ V  V /  __/ | (_| (_) | | | | | |  __/_|
+           \_/\_/ \___|_|\___\___/|_| |_| |_|\___(_)
+    ''')
+    
     while True: # Looppi joka kysyy käyttäjältä haluaako hän aloittaa pelin, jos vastaus on "yes" peli alkaa, jos "no" peli loppuu. Muussa tapauksessa kysytään uudelleen.
-        intro_prompt = input("Do you wish to Start the game? (yes/no): \n").lower()
-        if intro_prompt == "yes":
-            print("Great! Let's get started!")
-            break
-        elif intro_prompt == "no":
-            print("Goodbye!")
-            return
+        intro_input = input("Do you wish to Start the game? (yes/no): \n").lower()
+        if intro_input == "yes":
+            print("\nGreat! Let's get started!")
+            return True
+        elif intro_input == "no":
+            print("\nGoodbye!")
+            return False
         else:
-            print("Invalid input, please try again")
+            print("\nInvalid input, please try again:")
 
 
-def airplane_model_choice(): #Funktio lentokonemallin valintaan
-    airplane_model = input("Choose your airplane model: \n 1. Boeing 737 \n 2. Airbus A320 \n").lower()
-    if airplane_model == "1":
-        print("You have chosen the Boeing 737")
-    elif airplane_model == "2":
-        print("You have chosen the Airbus A320")
-    else:
-        print("Invalid input, please try again")
+def airplane_model_choice(): #Funktio lentokonemallin valintaan. Kysyy käyttäjältä haluamansa lentokonemallin ja tulostaa valinnan. Jos käyttäjä antaa väärän syötteen, pyytää syöttämään uudelleen.
+    while True:
+        airplane_model_input = input("Choose your airplane model (1-3): \n 1. Boeing 737 \n 2. Airbus A320 \n 3. JOTAIN VIELÄ \n").lower()
+        if airplane_model_input == "1":
+            print("You have chosen the Boeing 737.")
+            break
+        elif airplane_model_input == "2":
+            print("You have chosen the Airbus A320.")
+            break
+        elif airplane_model_input == "3":
+            print("You have chosen the JOTAIN VIELÄ.")
+            break
+        else:
+            print("\nInvalid input, please try again:")
+
 
 def intro_airplane_func(): #Funktio joka kutsuu molemmat "intro ja airplane_model_choice" funktiot <--- muutentaan myöhemmin main() funktioksi
-    intro()
-    airplane_model_choice()
+    if intro(): # Tässä kohtaa testataan, että jos alussa sanotaan "no" niin peli loppuu, muussa tapauksessa kutsutaan airplane_model_choice() funktiota.
+        airplane_model_choice()
     
+
 intro_airplane_func()
