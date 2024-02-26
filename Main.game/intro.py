f6820cca1eec8648ce855f92e2f949f2eac21d60
onnik <onni.kivinen@gmail.com>
2024-02-26 21:24:10 +0200

    tweaks

--- Main.game/intro.py
+++ Main.game/intro.py
@@ -1,23 +1,25 @@
 #-------------------------------------------------------------------------------
 # ONNIN SPAGETTI KOODIA
+
 from Asciiartfunc import welcome_text_to_ascii
 
+
 def intro(): # Intro funktio TestiTestiTesti
-    welcome_text_to_ascii()
+    welcome_text_to_ascii() # Kutsutaan Asciiartfunc.py tiedostosta welcome_text_to_ascii() funktiota, jolla saadaan hieno ASCII taideteos.
     while True: # Looppi joka kysyy käyttäjältä haluaako hän aloittaa pelin, jos vastaus on "yes" peli alkaa, jos "no" peli loppuu. Muussa tapauksessa kysytään uudelleen.
         intro_input = input("Do you wish to Start the game? (yes/no): \n").lower()
         if intro_input == "yes":
             print("\nGreat! Let's get started!")
-            return True
+            return True # Palauttaa True jos käyttäjä vastaa "yes" ja peli jatkuu.
         elif intro_input == "no":
             print("\nGoodbye!")
-            return False
+            return False # Palauttaa False jos käyttäjä vastaa "no" ja peli loppuu.
         else:
             print("\nInvalid input, please try again:")
 
 
 def airplane_model_choice(): #Funktio lentokonemallin valintaan. Kysyy käyttäjältä haluamansa lentokonemallin ja tulostaa valinnan. Jos käyttäjä antaa väärän syötteen, pyytää syöttämään uudelleen.
-    while True:
+    while True: 
         airplane_model_input = input("Choose your airplane model (1-3): \n 1. Boeing 737 \n 2. Airbus A320 \n 3. JOTAIN VIELÄ \n").lower()
         if airplane_model_input == "1":
             print("You have chosen the Boeing 737.")
@@ -32,9 +34,10 @@ def airplane_model_choice(): #Funktio lentokonemallin valintaan. Kysyy käyttäj
             print("\nInvalid input, please try again:")
 
 
-def intro_airplane_func(): #Funktio joka kutsuu molemmat "intro ja airplane_model_choice" funktiot <--- muutentaan myöhemmin main() funktioksi
+def intro_airplane_func(): #Funktio joka kutsuu molemmat "intro ja airplane_model_choice" funktiot <--- siirretään myöhemmin main() funktioon.
     if intro(): # Tässä kohtaa testataan, että jos alussa sanotaan "no" niin peli loppuu, muussa tapauksessa kutsutaan airplane_model_choice() funktiota.
-        airplane_model_choice()
+        airplane_model_choice() # Jos intro() palauttaa True, kutsutaan airplane_model_choice() funktiota.
     
 
-intro_airplane_func()
+intro_airplane_func() # Testataaan 
+
