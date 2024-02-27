e9a0903be935b0ffb333355d30f45290ed628142
eemilnu <eemil.nurmi@gmail.com>
2024-02-27 10:16:19 +0200

    Start code

--- Main.game/quiz.py
+++ Main.game/quiz.py
@@ -1,1 +1,12 @@
-m
+import random
+
+quiz_questions = {
+    "What is the capital of Macau" : "Itself"
+}
+
+random_answers = ["Opel", "Stockholm", "Chengdu"]
+
+def quiz_asker (quiz_questions):
+    length = len(quiz_question)
+    question = random.randint(0,length)
+    print(f"{quiz_questions[question]})
