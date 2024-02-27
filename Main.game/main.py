ee1aeadcb2ef7a53e5edd425b8ff05e6fcce8add
eemilnu <eemil.nurmi@gmail.com>
2024-02-27 09:22:05 +0200

    import and sql connector

--- Main.game/main.py
+++ Main.game/main.py
@@ -1,6 +1,13 @@
-"""
-bonjour
-Hallo, miehet
-Morjesta pöytään - Onni
+import Asciiartfunc.py
+import math
+from geopy.distance import geodesic
+import mysql.connector
 
-"""
+connection = mysql.connector.connect(
+    host="localhost",
+    port=3306,
+    database="flight_game",
+    user="root",
+    password="root",
+    autocommit=True
+)
