32e69c96e3e0313bd2fbe443c256dda73452e714
Patrik Skogberg <skogbergp@gmail.com>
2024-02-27 11:23:50 +0200

    new_score functio

--- Main.game/sql.py
+++ Main.game/sql.py
@@ -9,12 +9,20 @@ connection = mysql.connector.connect(
     autocommit=True
 )
 
-def search_large_airports():
-    sql = "SELECT * FROM airport where type = 'large_airport'"
+def search_large_airports(): # hakee isojen lentokenttien nimet ja sijainnin
+    sql = "SELECT name,latitude_deg,longitude_deg FROM airport where type = 'large_airport'"
     cursor = connection.cursor()
     cursor.execute(sql)
     airports = cursor.fetchall()
     if len(airports) == 0:
-        print("No airports found")
+        print("Airports not found")
     else:
+        print("Airports found")
         return airports
+
+def new_score(player_name,score): #tallentaa uuden pistemäärän tietokantaan
+    sql = f"INSERT INTO values {player_name},{score}"  #puuttuu taulu
+    cursor = connection.cursor()
+    cursor.execute(sql)
+    if cursor.rowcount == 1:
+        print("New score added")
