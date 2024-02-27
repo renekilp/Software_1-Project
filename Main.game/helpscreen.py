3bc5b168cbc570bfde26b1c36e2ae3e0085d9fb2
reneki <rene.kilpelainen@gmail.com>
2024-02-27 11:20:55 +0200

    added a new comment

---
+++ Main.game/helpscreen.py
@@ -0,0 +1,5 @@
+def helpscreen():
+    print("Welcome to the help screen!\nHere's a list of all available commands:")
+    print("\n1. Add a command\n2. Add a command\n3. Add a command\n4. Add a command")
+
+helpscreen()
