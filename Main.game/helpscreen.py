dd64fca8ca343e15bb153135734b1b0f2dc365df
reneki <rene.kilpelainen@gmail.com>
2024-02-27 12:32:32 +0200

    added new lines

--- Main.game/helpscreen.py
+++ Main.game/helpscreen.py
@@ -1,5 +1,14 @@
+from colorama import *
+
 def helpscreen():
-    print("Welcome to the help screen!\nHere's a list of all available commands:")
-    print("\n1. Add a command\n2. Add a command\n3. Add a command\n4. Add a command")
+    while True:
+        print(f"{Fore.GREEN}Welcome to the help screen!\nHere's a list of all available commands:")
+        print("\n1. Add a command\n2. Add a command\n3. Add a command\n4. Add a command")
+        input(f"\n{Fore.YELLOW}Please select a command:\n")
+    else:
+        print("")
+
+
+
 
 helpscreen()    #
