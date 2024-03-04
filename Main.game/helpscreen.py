5ff1523cb618e9f22413dad04979d37e1214bc33
reneki <rene.kilpelainen@gmail.com>
2024-03-04 13:39:20 +0200

    tweaked some line in helpscreen.py

--- Main.game/helpscreen.py
+++ Main.game/helpscreen.py
@@ -1,14 +1,27 @@
 from colorama import *
 
-def helpscreen():
+def help_screen():
+    commands = ['tutorial', 'player info', 'quit', 'credits']
     while True:
-        print(f"{Fore.GREEN}Welcome to the help screen!\nHere's a list of all available commands:")
-        print("\n1. Add a command\n2. Add a command\n3. Add a command\n4. Add a command")
-        input(f"\n{Fore.YELLOW}Please select a command:\n")
-    else:
-        print("")
+        print(f"{Fore.GREEN}Welcome to the help screen!\nHere's a list of all available commands:\n1. Add a command\n2. Add a command\n3. Add a command\n4. Add a command")
+        selected_command = input(f"\n{Fore.YELLOW}Please select a command:\n").lower()
+        if selected_command not in commands:
+            print("Invalid command! Please choose one of the available commands from the list.")
+        elif selected_command == 'tutorial':
+            tutorial_screen()
+        elif selected_command == 'player info':
+            player_info()
+        elif selected_command == 'quit':
+            quit_game()
+        elif selected_command == 'credits':
+            credits_screen()
 
 
 
 
-helpscreen()    #
+
+
+
+help_screen()
+
+#   exit, tutorial, player information, credits
