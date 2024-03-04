f2a2834088caeeaaa8013259752338683cfdd84b
onnik <onni.kivinen@gmail.com>
2024-03-04 14:34:54 +0200

    Commitattu Eemilin funktiot

--- Main.game/sql.py
+++ Main.game/sql.py
@@ -1,5 +1,6 @@
 import mysql.connector
-
+import random
+from geopy.distance import geodesic
 
 connection = mysql.connector.connect(
     host="localhost",
@@ -35,3 +36,27 @@ def new_score(player_name,score): #tallentaa uuden pistemäärän tietokantaan
     cursor.execute(sql)
     if cursor.rowcount == 1:
         print("New score added")
+
+def random_fly():
+    possible_airports = search_large_airports()
+    airport_number = random.randint(0,len(possible_airports)-1)
+    user_airport = possible_airports[airport_number]
+    return user_airport
+
+def travel_co2(user_airport, airplane_model_input):
+    next_airport = random_fly()
+    next_coordinates = [next_airport[1], next_airport[2]]
+    current_coordinates = [user_airport[1], user_airport[2]]
+    distance = geodesic(next_coordinates, current_coordinates).kilometers
+    if airplane_model_input == 1: #airbus a320
+        co2 = distance * 32.2 #grammaa per kilometri
+        flight_time = distance / 830 #kmh
+    elif airplane_model_input == 2: #boeing 737
+        co2 = distance * 115
+        flight_time = distance / 845
+    elif airplane_model_input == 3: #saab ja37 viggen
+        co2 = distance * 126
+        flight_time = distance / 2231
+    else:
+        print("Invalid airplane, cant calculate emission and distance")
+    return distance, co2, flight_time
