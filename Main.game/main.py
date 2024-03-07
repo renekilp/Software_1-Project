841315ff65655f955d507f1c63a072ac76b1b436
eemilnu <eemil.nurmi@gmail.com>
2024-03-07 12:14:46 +0200

    main game funktions

--- Main.game/main.py
+++ Main.game/main.py
@@ -11,9 +11,14 @@ import sql
 #import quiz <<- pitää kutsua tyyppisesti from quiz import quiz_asker tms. -onni
 
 from intro import intro #kutsutaan intro funktio tänne.
+from intor import airplane_model_choice
 
 # testataan main funktion toimintaa
 def main():
     intro()
 
 main()
+search = search_large_airports()
+airplane_model = airplane_model_choice()
+current_airport = random_fly()
+travel_co2(random_fly(), airplane_model)
