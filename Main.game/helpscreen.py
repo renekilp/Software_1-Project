7e900588439dcd4165fa974afe3a9ef0d574246f
reneki <rene.kilpelainen@gmail.com>
2024-03-07 13:43:25 +0200

    tweaked some lines

--- Main.game/helpscreen.py
+++ Main.game/helpscreen.py
@@ -27,21 +27,36 @@ def help_screen():
 def tutorial_screen():
     tutorial_ascii()
     tut_text = """
+    Heya! I'm your friendly co-pilot AI O.N.N.I (Omnipotent, Neat, Navigation, Instructor)!
+    I'm here to give you some nifty information about the game.
+    
     First you have to choose a plane. The only difference between the planes is the co2 usage (and your own preference).
     Your main goal is to hop from airport to airport. You as a player is bombarded with offbeat trivia questions about each country you visit.
     Your journey will be filled with laughter and learning. But remember, with only one wrong answer, your plane will make an emergency landing 
     ending your journey...
     
-    How far can you reach without making a mistake? Let's find out!
     After the game ends, your will be scored based on the points, distance and co2 usage.
+    How far can you reach without making a mistake? Let's find out! YOU GOT THIS BOSS!
     
-    Remember that you can always use the 'help' command if you find yourself stuck.
+    Remember that you can always use the 'help' command if you find yourself stuck. I'm here to help!
     """
     slowly_generate_print(tut_text, delay=0.015)
 
 
 def player_info():
     playerinfo_ascii()
+    print(f"""
+    Welcome BOSS! I've been tracking some of your progress...
+    
+    Your current score is: {score}
+    Your current flown distance is: {distance}
+    Your current fly time is: {used_time}
+    Your current co2 usage is: {used_co2}
+    
+    Wowee! You are flying like a real captain! Let's push that score up.
+    """)
+    
+    
 
 
 
