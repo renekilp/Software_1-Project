3024374ccc2f4969fcdd118f4cb4ec6bc34928da
Skogbergp <skogbergp@gmail.com>
2024-03-08 20:53:39 +0200

    new function to get random question

--- Main.game/gamesql.py
+++ Main.game/gamesql.py
@@ -13,7 +13,7 @@ connection = mysql.connector.connect(
 def query_database(sql,fetchall=True): #Tein täst funktion, niin ei tarvi aina tehä cursor ja execute
     cursor = connection.cursor()
     cursor.execute(sql)
-    
+
     if fetchall:
         return cursor.fetchall()
     else:
@@ -24,15 +24,21 @@ def query_database(sql,fetchall=True): #Tein täst funktion, niin ei tarvi aina 
 def search_large_airports(): # hakee isojen lentokenttien nimet ja sijainnin
     sql = "SELECT name,latitude_deg,longitude_deg FROM airport where type = 'large_airport'"
     airports = query_database(sql) # airports = suoritetaan query_database funktio, joka hakee tietokannasta tiedot annetulla sql muuttujalla
-    if len(airports) == 0:
-        pass
-    else:
-        return airports
+
+    return airports
+
+def get_random_question():
+    question_count = query_database("SELECT COUNT(*) FROM questions")
+    random_id = random.randint(1, question_count[0][0])
+    sql = f"SELECT question,correct_answer,wrong_answer_1,wrong_answer_2,wrong_answer_3,wrong_answer_4 FROM questions where id = {random_id}"
+    query = query_database(sql)
+    return query
 
 def new_score(player_name,score): #tallentaa uuden pistemäärän tietokantaan
     sql = f"INSERT INTO values {player_name},{score}"  #puuttuu taulu
     cursor = connection.cursor()
     cursor.execute(sql)
+    cursor.close()
     if cursor.rowcount == 1:
         print("New score added")
 
