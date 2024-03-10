8caca58367e6a6df63c3753bd90deb31541b9ab4
onnik <onni.kivinen@gmail.com>
2024-03-10 13:10:10 +0200

    tweak

--- Database/highscore_into_database.py
+++ Database/highscore_into_database.py
@@ -15,8 +15,8 @@ def high_score_taulun_luonti():
     kursori.execute('''
             CREATE TABLE IF NOT EXISTS high_score 
             (id INTEGER PRIMARY KEY AUTO_INCREMENT,
-            player_name TEXT,
-            player_score TEXT)''')
+            player_name VARCHAR(255),
+            player_score INTEGER)''')
     
     yhteys.commit()
     kursori.close()
@@ -24,4 +24,8 @@ def high_score_taulun_luonti():
 
 high_score_taulun_luonti()
 
+kursori = yhteys.cursor()
 
+def lisaa_high_score_tauluun(player_name, player_score):
+    kursori.execute("INSERT INTO high_score (player_name, player_score) VALUES (%s, %s)",
+                    (player_name, player_score))
