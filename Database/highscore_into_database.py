859bd82abe1c8708709f431acb29d2d5a70ad968
onnik <onni.kivinen@gmail.com>
2024-03-10 13:14:54 +0200

    lisätty funktio jolla lisätään peyman tauluun pisteillä 100

--- Database/highscore_into_database.py
+++ Database/highscore_into_database.py
@@ -24,8 +24,17 @@ def high_score_taulun_luonti():
 
 high_score_taulun_luonti()
 
-kursori = yhteys.cursor()
+
 
 def lisaa_high_score_tauluun(player_name, player_score):
+    player_name = "Peyman"
+    player_score = 100
+    
+    kursori = yhteys.cursor()
     kursori.execute("INSERT INTO high_score (player_name, player_score) VALUES (%s, %s)",
                     (player_name, player_score))
+    yhteys.commit()
+    kursori.close()
+    print("2. Pelaaajan nimi ja pisteet lisätty tauluun.")
+
+lisaa_high_score_tauluun("Peyman", 100)
