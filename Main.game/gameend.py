552cbf7951a799a3147de453e61e4b5cbb590b6f
Skogbergp <skogbergp@gmail.com>
2024-03-11 23:29:59 +0200

    commented

--- Main.game/gameend.py
+++ Main.game/gameend.py
@@ -1,7 +1,7 @@
 from gamesql import new_score
 
 
-def endgame(score):
+def endgame(score): # pelin loppuun sijoittuvat functiot
     player_name = input("What is your name? \n")
     score = int(score)
     new_score(player_name, score)
