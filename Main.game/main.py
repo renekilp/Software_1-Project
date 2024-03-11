341471959c99ee2950fb6307987c63a14826fc8a
Patrik Skogberg <skogbergp@gmail.com>
2024-03-11 14:02:04 +0200

    small fixes and main.py cleaning

--- Main.game/main.py
+++ Main.game/main.py
@@ -23,39 +23,26 @@ if gameintro.starting_screen(): # aloitetaanko peli vai ei
 
         # pelaajan komento functiot
 
-        print(f"{Fore.GREEN}Your current location is: {current_airport[0]}{Fore.RESET}")
-        gamesql.travel_co2(current_airport, airplane_model)
-        print(f"{Fore.GREEN}CO2 used: {co2_used}g, Distance traveled: {distance}km, Time used: {used_time}h{Fore.RESET}")
+        if help_screen():
+            score += 1     # lisää pisteen oikein vastatusta kysymyksestä
+            travel_info = gamesql.travel_co2(current_airport, airplane_model) # lentoon liittyvät tiedot
+            distance += travel_info[0]
+            co2_used += travel_info[1]
+            used_time += travel_info[2]
+            current_airport = travel_info[3]
 
-
-        if game_going:
-
-            if help_screen():
-                score += 1     # lisää pisteen oikein vastatusta kysymyksestä
-                travel_info = gamesql.travel_co2(current_airport, airplane_model) # lentoon liittyvät tiedot
-                distance += travel_info[0]
-                co2_used += travel_info[1]
-                used_time += travel_info[2]
-                current_airport = travel_info[3]
-
-                continue_game = input(f"{Fore.GREEN}Do you want to continue? (y/n):\n{Fore.RESET}").lower()
-                if continue_game == "y":
-                    game_going = True
-                elif continue_game == "n":
-                    game_going = False
-                else:
-                    print(f"{Fore.YELLOW}Invalid input entered")
-
+            continue_game = input(f"{Fore.GREEN}Do you want to continue? (y/n):\n{Fore.RESET}").lower()
+            if continue_game == "y":
+                game_going = True
+            elif continue_game == "n":
+                game_going = False
             else:
-                print(f"{Fore.GREEN}Your final score is: {score}{Fore.RESET}")
-                print(f"Your final co2 and how long your flighttime is: \n {co2_used:.2f}gramms and {used_time % 24:.2f}days")
-                gamecredits.print_game_credits()
-                game_going = False # lopettaa pelin puuttuu pelin loppuun kuuluvat funktiot
+                print(f"{Fore.YELLOW}Invalid input entered")
+
         else:
             print(f"{Fore.GREEN}Your final score is: {score}{Fore.RESET}")
             print(f"Your final co2 and how long your flighttime is: \n {co2_used:.2f}gramms and {used_time % 24:.2f}days")
             gamecredits.game_credits_query()
             gamesql.top_players()
             game_going = False # lopettaa pelin puuttuu pelin loppuun kuuluvat funktiot
-
 #
