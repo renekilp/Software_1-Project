3f7f4a70937825d2f179fe4c0e2bb65a1314e8ee
Patrik Skogberg <skogbergp@gmail.com>
2024-03-11 13:04:05 +0200

    fixed do you watnt to continue

--- Main.game/main.py
+++ Main.game/main.py
@@ -15,25 +15,24 @@ current_airport = gamesql.random_fly()
 game_going = True
 
 if gameintro.starting_screen(): # aloitetaanko peli vai ei
-    airplane_model = gameintro.airplane_model_choice()
+
+    airplane_model = gameintro.airplane_model_choice() # kysyy ja tallentaa lentokoneen
 
     while game_going:
 
         # pelaajan komento functiot
 
-        gamesql.random_fly()
-        print(f"{Fore.GREEN}Your current location is: {current_airport}{Fore.RESET}")
+        print(f"{Fore.GREEN}Your current location is: {current_airport[0]}{Fore.RESET}")
         gamesql.travel_co2(current_airport, airplane_model)
         print(f"{Fore.GREEN}CO2 used: {co2_used}g, Distance traveled: {distance}km, Time used: {used_time}h{Fore.RESET}")
 
-        while quizv2.question_query_from_database():
+        if quizv2.question_query_from_database():
             score += 1     # lisää pisteen oikein vastatusta kysymyksestä
             travel_info = gamesql.travel_co2(current_airport, airplane_model) # lentoon liittyvät tiedot
             distance += travel_info[0]
             co2_used += travel_info[1]
             used_time += travel_info[2]
             current_airport = travel_info[3]
-
 
             continue_game = input(f"{Fore.GREEN}Do you want to continue? (y/n):\n{Fore.RESET}").lower()
             if continue_game == "y":
