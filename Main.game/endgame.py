8eb51ee724a0beb5f8e16eb4b2bd049bc8a59548
Skogbergp <skogbergp@gmail.com>
2024-03-12 15:17:13 +0200

    looping yes no

--- Main.game/endgame.py
+++ Main.game/endgame.py
@@ -9,10 +9,15 @@ def end_game(score,co2_used,used_time):
     print(f"Your final co2 and your flight time is: \n {co2_used:.2f} gramms and {used_time / 24:.2f} days")
 
     gamesql.top_players()
-    if input("\nWould you like to save your score? (y/n) \n").lower() == "y":
-        gamesql.new_score(player_name=input("Enter your name\n"),score = score)
-    else:
-        gamecredits.game_credits_query()
-        print("Thanks for playing!")
-
+    while True:
+        player_input = input("\nWould you like to save your score? (y/n) \n").lower()
+        if player_input == "y":
+            gamesql.new_score(player_name=input("Enter your name\n"),score = score)
+            break
+        elif player_input == "n":
+            gamecredits.game_credits_query()
+            print("Thanks for playing!")
+            break
+        else:
+            print("Invalid input")
     exit()
